4ff6f75d7d35a70f3337ce79f7788e94bcfbb943
madrodgerflynn <madrodgerflynn@gmail.com>
2022-11-29 13:14:48 -0700

    working on bio.js

---
+++ brokenmetaljewelry/src/components/bio/Bio.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Bio.css";
+
+
+function Bio() {
+     return (
+         <div className="mainContainer">
+            <div className="bioContainer">
+                <h1>About Me</h1>
+            </div>
+
+        </div>
+     )
+}
