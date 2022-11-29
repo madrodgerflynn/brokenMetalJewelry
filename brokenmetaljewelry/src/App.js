22ed3118027171f8591945f46bc44f84b1dd282c
madrodgerflynn <madrodgerflynn@gmail.com>
2022-11-29 14:12:36 -0700

    working on bio.js

--- brokenmetaljewelry/src/App.js
+++ brokenmetaljewelry/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Bio from "./components/bio/Bio "
+import Works from "./components/works/Works"
+import Contact from "./components/contact/Contact--"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <NavBar /> */}
+
+      <Routes>
+        <Route path="/reactFolio" element={<Bio />} />
+        <Route path="/" element={<Bio />} />
+        <Route path="/Bio" element={<Bio />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Works" element={<Works />} />
+      </Routes>
+    </Router>
   );
 }
 
--- brokenmetaljewelry/src/components/bio/Bio.js
+++ brokenmetaljewelry/src/components/bio/Bio.js
@@ -7,8 +7,15 @@ function Bio() {
          <div className="mainContainer">
             <div className="bioContainer">
                 <h1>About Me</h1>
+                <p>
+                    Hello, my name is Tyler Anderson and this is my page. I figure I'll give you a little information about what I do. Working in the manufacturing/eCommerce industry I found all sorts of interesting scraps. It is with these scraps that I built my first necklace. From there it was all uphill. The training and practice has been difficult but incredibly enlightening. I aim to use scraps, to make things that seem trivial, beautiful. 
+                </p>
+            </div>
+            <div className="picCarousel">
+
             </div>
 
         </div>
      )
 }
+export default Bio
