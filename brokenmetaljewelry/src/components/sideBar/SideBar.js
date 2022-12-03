f8b5f8b138dc6f200a1736dbc4e5c8fd77846a4c
madrodgerflynn <madrodgerflynn@gmail.com>
2022-12-03 12:45:10 -0700

    workind on Bio.css

---
+++ brokenmetaljewelry/src/components/sideBar/SideBar.js
@@ -0,0 +1,18 @@
+import "./SideBar.css";
+import React from "react";
+import {Link} from "react-router-dom";
+
+function SideBar (){
+    return(
+        <>
+        <div className="sideBarContainer">
+            <div className="sideBar">
+                <Link to={{pathname: "../contact"}}>Contact Me</Link>
+                <Link to={{pathname: "../works"}}>My Pieces</Link>
+            </div>
+
+        </div>
+        </>
+    )
+}
+export default SideBar
