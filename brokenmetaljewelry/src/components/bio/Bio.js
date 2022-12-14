c4527fbd25492b9eb915614f35b7bca2c8c951d8
madrodgerflynn <madrodgerflynn@gmail.com>
2022-12-13 17:30:56 -0700

    small change in bio

--- brokenmetaljewelry/src/components/bio/Bio.js
+++ brokenmetaljewelry/src/components/bio/Bio.js
@@ -20,7 +20,7 @@ function Bio() {
             <div className="bioContainer">
                 <h1>About Me</h1>
                 <p>
-                    Hello, my name is Tyler Anderson and this is my page. I figure I'll give you a little information about what I do. Working in the manufacturing/eCommerce industry I found all sorts of interesting scraps. It is with these scraps that I built my first necklace. From there it was all uphill. The training and practice has been difficult but incredibly enlightening. I aim to use scraps, to make things that seem trivial, beautiful. 
+                    Hello, my name is Tyler Anderson and this is my page. I figure I'll give you a little information about what I do. Working in the manufacturing/eCommerce industry I found all sorts of interesting scraps. It is with these scraps that I built my first necklace. From there it was all uphill. The training and practice has been difficult but incredibly enlightening. I aim to use scraps, to make things that seem trivial, beautiful. I know it sounds cheesy, but it has become a therapy in this crazy world we live in! 
                 </p>
             </div>
             <div className="picCarousel">
