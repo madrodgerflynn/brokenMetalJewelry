6474a53d87d6740fe75353cc5729ad35f73a188a
madrodgerflynn <madrodgerflynn@gmail.com>
2022-12-16 11:42:32 -0700

    adding works to page

--- brokenmetaljewelry/src/App.js
+++ brokenmetaljewelry/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Bio from "./components/bio/Bio";
-import particles from "../particlesjs-config.json"
+// import particles from "../particlesjs-config.json"
 import Works from "./components/works/Works";
 // import Contact from "./components/contact/Contact";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
---
+++ brokenmetaljewelry/src/components/works/Works.js
@@ -0,0 +1,11 @@
+function Works () {
+    
+    return (
+        <div className="worksContainer">
+            <div className="worksHeader">
+                <h1>Check Out My Stuff!</h1>
+            </div>
+            <div></div>
+        </div>
+        )
+    }
