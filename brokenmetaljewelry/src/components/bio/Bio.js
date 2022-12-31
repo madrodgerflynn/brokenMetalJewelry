a63b3b692f50577c220e69a3d41e125a689c7d24
madrodgerflynn <madrodgerflynn@gmail.com>
2022-12-30 18:25:54 -0700

    fixed some bugs in the background

--- brokenmetaljewelry/src/components/bio/Bio.js
+++ brokenmetaljewelry/src/components/bio/Bio.js
@@ -22,8 +22,10 @@ function Bio() {
             <div className="bioContainer">
                 <h1>About Me</h1>
                 <p className="bioText">
-                    Hello, my name is Tyler Anderson and this is my page. I figure I'll give you a little information about what I do. Working in the manufacturing/eCommerce industry I found all sorts of interesting scraps. It is with these scraps that I built my first necklace. From there it was all uphill. The training and practice has been difficult but incredibly enlightening. I aim to use scraps, to make things that seem trivial, beautiful. I know it sounds cheesy, but it has become a therapy in this crazy world we live in! 
+                    Hello, my name is Tyler Anderson and this is my page. I figure I'll give you a little information about what I do. Working in the manufacturing/eCommerce industry I found all sorts of interesting scraps. It is with these scraps that I built my first necklace. From there it was all uphill. The training and practice has been difficult but incredibly enlightening. I aim to use scraps, to make things that seem trivial, beautiful. I know it sounds cheesy, but it has become a therapy in this crazy world we live in!
                 </p>
+                <br></br>
+                <p className="additionalInfo">Here are some pictures of me working at the Denver Art Gym! I have included a link HERE to their site. Please check it out because it is an incredible place!</p>
             </div>
             <div className="picCarousel">
                
@@ -33,8 +35,6 @@ function Bio() {
             <div className="myPictures">
                 <p className="pictureDescription">{picture.description}</p>
                 <img className="imagesOfMe"src={picture.picLink} alt={picture.alt} ></img>
-
-            <p className="additionalInfo">Here are some pictures of me working at the Denver Art Gym! I have included a link HERE to their site. Please check it out because it is an incredible place!</p>
             </div>
         </div>
      )
