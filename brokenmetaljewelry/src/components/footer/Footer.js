0970434125fbb688f4c8f47718d458cf01a62b4b
madrodgerflynn <madrodgerflynn@gmail.com>
2023-01-01 17:12:16 -0700

    adding a footer

---
+++ brokenmetaljewelry/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer () {
+    return (
+        <div className="footerContainer">
+            <i className="GiGearHammer"></i>
+            <i className="HiWrenchScrewdriver"></i>
+        </div>
+    )
+}
+export default Footer;
