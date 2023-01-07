de46b5563a34acc637ca50eb6a9c75360da29343
madrodgerflynn <madrodgerflynn@gmail.com>
2023-01-07 16:30:46 -0700

    setting up emailJS

--- brokenmetaljewelry/src/components/contact/Contact.js
+++ brokenmetaljewelry/src/components/contact/Contact.js
@@ -2,9 +2,26 @@ import React, {useState} from "react";
 import "./Contact.css";
 
 function Contact(){
+    let[senderName, setSenderName] = useState("");
+    let[senderEmail, setSenderEmail] = useState("");
+    let[message, setMessage] = useState("");
+
+    function updateInputState(event) {
+        switch (event.target.name) {
+            case "sender_name":
+                setSenderName(event.target.value);
+            break;
+            case "sender_email":
+                setSenderEmail(event.target.value);
+                break;
+                case "message":
+                    setMessage(event.target.value);
+                    break;
+        }
+    }
     return(
         <div className="contactContainer">
-            <form className="row contactForm"></form>
+            <form className="row contactForm" onSubmit={handleEvent}></form>
         </div>
     )
 }
