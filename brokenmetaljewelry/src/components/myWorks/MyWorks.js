475f111f97d69d9e7e4f1df9e29d6d24172eaf3b
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-08 19:49:24 -0700

    beta card positioned

--- brokenmetaljewelry/src/components/myWorks/MyWorks.js
+++ brokenmetaljewelry/src/components/myWorks/MyWorks.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./MyWorks.css";
-import "../workimages/moonScape.png"
+import moonScape from "../workimages/moonScape.png"
+
 
 function MyWorks(){
    
     return(
-        <body>
-        <div className="card1" style={{ width: '18rem' }}>
-        <img orientation="top" src={"moonScape.png"} alt="Moonscape Neckalce" />
+        <body className="card-body">
+        <div className="card1" >
+        <img orientation="top" src={moonScape} alt="Moonscape Neckalce" />
           <h1>MoonScape Necklace</h1>
           <p>
             Some quick example text to build on the card title and make up the bulk of the card's content.
