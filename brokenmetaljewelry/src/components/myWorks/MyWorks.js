2320fd19668052de894db40abb719feb264e4cbe
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-08 18:02:22 -0700

    working on MyWorks.css

--- brokenmetaljewelry/src/components/myWorks/MyWorks.js
+++ brokenmetaljewelry/src/components/myWorks/MyWorks.js
@@ -3,18 +3,18 @@ import "./myWorks.css";
 import "../images/moonScape.png"
 
 function myWorks(){
-    let CCard 
+   
     return(
         <div className="card1" style={{ width: '18rem' }}>
-        <img orientation="top" src={"moonScape.png"} />
+        <img orientation="top" src={"moonScape.png"} alt="" />
         <body>
           <h1>MoonScape Necklace</h1>
           <p>
             Some quick example text to build on the card title and make up the bulk of the card's content.
           </p>
-          <CButton href="#">Go somewhere</CButton>
-        </CCardBody>
-      </CCard> 
+          <button href="#">Go somewhere</button>
+        </body>
+      </div> 
     )
 }
 export default myWorks;
