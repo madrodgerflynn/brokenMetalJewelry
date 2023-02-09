559530307c3c3d2b2b41ccddcbb783e1e3d7f1ca
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-08 17:19:24 -0700

    working on MyWorks.js

---
+++ brokenmetaljewelry/src/components/myWorks/MyWorks.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./myWorks.css";
+import "../images/moonScape.png"
+
+function myWorks(){
+    let CCard 
+    return(
+        <CCard style={{ width: '18rem' }}>
+        <CCardImage orientation="top" src={"moonScape.png"} />
+        <CCardBody>
+          <CCardTitle>Card title</CCardTitle>
+          <CCardText>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </CCardText>
+          <CButton href="#">Go somewhere</CButton>
+        </CCardBody>
+      </CCard> 
+    )
+}
