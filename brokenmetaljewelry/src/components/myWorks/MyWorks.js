de48a3265454cdb8c19a2cea2d4756115b67faf2
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-08 19:34:01 -0700

    getting images to show

--- brokenmetaljewelry/src/components/myWorks/MyWorks.js
+++ brokenmetaljewelry/src/components/myWorks/MyWorks.js
@@ -5,16 +5,16 @@ import "../workimages/moonScape.png"
 function MyWorks(){
    
     return(
-        <div className="card1" style={{ width: '18rem' }}>
-        <img orientation="top" src={"moonScape.png"} alt="" />
         <body>
+        <div className="card1" style={{ width: '18rem' }}>
+        <img orientation="top" src={"moonScape.png"} alt="Moonscape Neckalce" />
           <h1>MoonScape Necklace</h1>
           <p>
             Some quick example text to build on the card title and make up the bulk of the card's content.
           </p>
           <button href="#">Go somewhere</button>
-        </body>
       </div> 
+        </body>
     )
 }
 export default MyWorks;
