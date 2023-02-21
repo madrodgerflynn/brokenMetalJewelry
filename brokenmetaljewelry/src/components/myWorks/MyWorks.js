027cf41bea031476dd2b9d68d4e9983421e30496
madrodgerflynn <madrodgerflynn@gmail.com>
2023-02-20 17:35:50 -0700

    powers gonna surge adding another card

--- brokenmetaljewelry/src/components/myWorks/MyWorks.js
+++ brokenmetaljewelry/src/components/myWorks/MyWorks.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./MyWorks.css";
-import moonScape from "../workimages/moonScape.png"
-import tieFighters from "../workimages/tieFighters.png"
+import moonScape from "../workimages/moonScape.png";
+import tieFighters from "../workimages/tieFighters.png";
+import blueSteel from "../workimages/blueSteel.png";
 
 function MyWorks(){
    
@@ -20,6 +21,16 @@ function MyWorks(){
           <br></br>
           {/* <card></card> */}
         <img className="card-image"orientation="top" src={tieFighters} alt="Tie Figher Earings"/>
+          <h1>Tie Fighter Earings</h1>
+          <p>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <button href="#">Go somewhere</button>
+      </div> 
+        <div className="card1" >
+          <br></br>
+          {/* <card></card> */}
+        <img className="card-image"orientation="top" src={blueSteel} alt="Tie Figher Earings"/>
           <h1>Tie Fighter Earings</h1>
           <p>
             Some quick example text to build on the card title and make up the bulk of the card's content.
