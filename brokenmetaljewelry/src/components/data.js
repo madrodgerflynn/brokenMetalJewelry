52b2f23662490c2b08362a9ec2738cfb949a15ad
madrodgerflynn <madrodgerflynn@gmail.com>
2023-03-22 09:30:25 -0600

    adding data for purchases

--- brokenmetaljewelry/src/components/data.js
+++ brokenmetaljewelry/src/components/data.js
@@ -1,8 +1,20 @@
 const data= {
-    products: [{
-        id: 'item1',
+    products: [
+        {id: 'item1',
         name: 'Moonscape Necklace',
         price: 120,
+   
+    },    
+   
+        { id: 'item2',
+        name: 'Tie Fighter Earings',
+        price: 120,
+  
+    },   
+    
+        {id: 'item3',
+        name: 'Blue Steel',
+        price: 120,
     }
-    ]    
+  ],  
 }
