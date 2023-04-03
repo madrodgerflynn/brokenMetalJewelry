125410fd587f567b9fa420410853bb6e093ae487
madrodgerflynn <madrodgerflynn@gmail.com>
2023-04-03 06:47:10 -0600

    adding links

--- brokenmetaljewelry/src/components/purchases/Product.js
+++ brokenmetaljewelry/src/components/purchases/Product.js
@@ -3,14 +3,12 @@ import "./Purchase.css";
 // import data from "../data";
 
 function Product(props) {
- const {products} = props;
+//  const {products} = props;
  return(
     <div className="purchaseForm">
         <h2>Your Purchase</h2>
         <div className="row">
-            {products.map((product)=>(
-                <Product key={product.id} product={product}></Product>
-            ))}
+            
         </div>
     </div>
  )
