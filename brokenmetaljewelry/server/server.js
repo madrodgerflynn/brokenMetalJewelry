6552e162ccec6c00235c9f246beb403ac424ecf6
madrodgerflynn <madrodgerflynn@gmail.com>
2023-05-08 16:24:57 -0600

    working on server

--- brokenmetaljewelry/server/server.js
+++ brokenmetaljewelry/server/server.js
@@ -1,5 +1,13 @@
 const mongoose = require ('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb:'
-)
+    process.env.MONGODB_URI ||'mongodb+srv://madrodgerflynn:madrodgerflynn@brokenmetaljewerly.vcoxikx.mongodb.net/',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    }
+
+);
+
+module.exports = mongoose.connection;
