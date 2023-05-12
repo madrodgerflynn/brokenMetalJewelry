2d5f39e256799a615fc925ef383b022f9c2e3499
madrodgerflynn <madrodgerflynn@gmail.com>
2023-05-12 06:27:51 -0600

    server stuff

--- brokenmetaljewelry/server/connection.js
+++ brokenmetaljewelry/server/connection.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config()
+if(process.env.MONGODB_CONNECTION_STRING){
+  console.log('> Connecting too process.env.MONGODB_CONNECTION_STRING')
+} else {
+  console.log('> Connecting to Local DB');
+}
 mongoose.connect(
-  process.env.MONGODB_URI || '',
+  process.env.MONGODB_CONNECTION_STRING  || 'mongodb://127.0.0.1:27017/truckr',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
